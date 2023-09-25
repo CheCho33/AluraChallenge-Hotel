131778908885e32f8bb9ce82abfef74678ad3e92
Checho_Tabares <sergio_andrestb@yahoo.es>
2023-09-24 21:02:43 -0500

    organizando filtro de seguridad y pagina loggin (aun no completo)

---
+++ src/main/java/com/challenge/alura/hotel/repository/HuespedRepository.java
@@ -0,0 +1,11 @@
+package com.challenge.alura.hotel.repository;
+
+import com.challenge.alura.hotel.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByLogin(String userName);
+
+}
